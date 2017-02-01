519f45cc47238254cf7364d1556da22042eef51a
orlando.rzambello <orlando.rzambello@AME0556356W10-1>
2017-02-01 21:15:24 -0200

    Add project files.

---
+++ Agenda/Formularios/frmAgenda.Designer.cs
@@ -0,0 +1,395 @@
+﻿namespace Agenda
+{
+    partial class frmAgenda
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAgenda));
+            this.grpClientes = new System.Windows.Forms.GroupBox();
+            this.label9 = new System.Windows.Forms.Label();
+            this.mskCelular = new System.Windows.Forms.MaskedTextBox();
+            this.btnCEP = new System.Windows.Forms.Button();
+            this.label8 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.lblNome = new System.Windows.Forms.Label();
+            this.mskCEP = new System.Windows.Forms.MaskedTextBox();
+            this.cboEstado = new System.Windows.Forms.ComboBox();
+            this.txtCidade = new System.Windows.Forms.TextBox();
+            this.txtBairro = new System.Windows.Forms.TextBox();
+            this.txtNumero = new System.Windows.Forms.TextBox();
+            this.txtEndereco = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCodigo = new System.Windows.Forms.TextBox();
+            this.errErro = new System.Windows.Forms.ErrorProvider(this.components);
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tstSalvar = new System.Windows.Forms.ToolStripButton();
+            this.tstExcluir = new System.Windows.Forms.ToolStripButton();
+            this.tstPesquisar = new System.Windows.Forms.ToolStripButton();
+            this.toolStripSeparator1 = new System.Windows.Forms.ToolStripSeparator();
+            this.tstSair = new System.Windows.Forms.ToolStripButton();
+            this.grpClientes.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errErro)).BeginInit();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grpClientes
+            // 
+            this.grpClientes.Controls.Add(this.label9);
+            this.grpClientes.Controls.Add(this.mskCelular);
+            this.grpClientes.Controls.Add(this.btnCEP);
+            this.grpClientes.Controls.Add(this.label8);
+            this.grpClientes.Controls.Add(this.label7);
+            this.grpClientes.Controls.Add(this.label6);
+            this.grpClientes.Controls.Add(this.label5);
+            this.grpClientes.Controls.Add(this.label4);
+            this.grpClientes.Controls.Add(this.label3);
+            this.grpClientes.Controls.Add(this.lblNome);
+            this.grpClientes.Controls.Add(this.mskCEP);
+            this.grpClientes.Controls.Add(this.cboEstado);
+            this.grpClientes.Controls.Add(this.txtCidade);
+            this.grpClientes.Controls.Add(this.txtBairro);
+            this.grpClientes.Controls.Add(this.txtNumero);
+            this.grpClientes.Controls.Add(this.txtEndereco);
+            this.grpClientes.Controls.Add(this.txtNome);
+            this.grpClientes.Controls.Add(this.label1);
+            this.grpClientes.Controls.Add(this.txtCodigo);
+            this.grpClientes.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.grpClientes.Location = new System.Drawing.Point(12, 64);
+            this.grpClientes.Name = "grpClientes";
+            this.grpClientes.Size = new System.Drawing.Size(583, 177);
+            this.grpClientes.TabIndex = 1;
+            this.grpClientes.TabStop = false;
+            this.grpClientes.Text = "Dados do Cliente";
+            // 
+            // label9
+            // 
+            this.label9.AutoSize = true;
+            this.label9.Location = new System.Drawing.Point(462, 129);
+            this.label9.Name = "label9";
+            this.label9.Size = new System.Drawing.Size(46, 13);
+            this.label9.TabIndex = 18;
+            this.label9.Text = "Celular";
+            // 
+            // mskCelular
+            // 
+            this.mskCelular.Location = new System.Drawing.Point(465, 145);
+            this.mskCelular.Mask = "(00) 00000-0000";
+            this.mskCelular.Name = "mskCelular";
+            this.mskCelular.Size = new System.Drawing.Size(112, 20);
+            this.mskCelular.TabIndex = 9;
+            // 
+            // btnCEP
+            // 
+            this.btnCEP.Image = ((System.Drawing.Image)(resources.GetObject("btnCEP.Image")));
+            this.btnCEP.Location = new System.Drawing.Point(102, 94);
+            this.btnCEP.Name = "btnCEP";
+            this.btnCEP.Size = new System.Drawing.Size(27, 23);
+            this.btnCEP.TabIndex = 3;
+            this.btnCEP.UseVisualStyleBackColor = true;
+            this.btnCEP.Click += new System.EventHandler(this.btnCEP_Click);
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(16, 80);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(31, 13);
+            this.label8.TabIndex = 15;
+            this.label8.Text = "CEP";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(416, 128);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(46, 13);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "Estado";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(186, 129);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(46, 13);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Cidade";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(16, 129);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Bairro";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(527, 80);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(50, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Número";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(136, 80);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(61, 13);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Endereço";
+            // 
+            // lblNome
+            // 
+            this.lblNome.AutoSize = true;
+            this.lblNome.Location = new System.Drawing.Point(96, 29);
+            this.lblNome.Name = "lblNome";
+            this.lblNome.Size = new System.Drawing.Size(39, 13);
+            this.lblNome.TabIndex = 9;
+            this.lblNome.Text = "Nome";
+            // 
+            // mskCEP
+            // 
+            this.mskCEP.Location = new System.Drawing.Point(19, 96);
+            this.mskCEP.Mask = "00000-000";
+            this.mskCEP.Name = "mskCEP";
+            this.mskCEP.Size = new System.Drawing.Size(77, 20);
+            this.mskCEP.TabIndex = 2;
+            // 
+            // cboEstado
+            // 
+            this.cboEstado.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboEstado.FormattingEnabled = true;
+            this.cboEstado.Items.AddRange(new object[] {
+            "SP",
+            "MG",
+            "BA",
+            "RJ",
+            "PR"});
+            this.cboEstado.Location = new System.Drawing.Point(419, 144);
+            this.cboEstado.Name = "cboEstado";
+            this.cboEstado.Size = new System.Drawing.Size(40, 21);
+            this.cboEstado.TabIndex = 8;
+            // 
+            // txtCidade
+            // 
+            this.txtCidade.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtCidade.Location = new System.Drawing.Point(189, 145);
+            this.txtCidade.MaxLength = 50;
+            this.txtCidade.Name = "txtCidade";
+            this.txtCidade.Size = new System.Drawing.Size(224, 20);
+            this.txtCidade.TabIndex = 7;
+            // 
+            // txtBairro
+            // 
+            this.txtBairro.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtBairro.Location = new System.Drawing.Point(19, 145);
+            this.txtBairro.MaxLength = 30;
+            this.txtBairro.Name = "txtBairro";
+            this.txtBairro.Size = new System.Drawing.Size(164, 20);
+            this.txtBairro.TabIndex = 6;
+            // 
+            // txtNumero
+            // 
+            this.txtNumero.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNumero.Location = new System.Drawing.Point(530, 96);
+            this.txtNumero.MaxLength = 5;
+            this.txtNumero.Name = "txtNumero";
+            this.txtNumero.Size = new System.Drawing.Size(47, 20);
+            this.txtNumero.TabIndex = 5;
+            this.txtNumero.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // txtEndereco
+            // 
+            this.txtEndereco.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtEndereco.Location = new System.Drawing.Point(139, 96);
+            this.txtEndereco.MaxLength = 50;
+            this.txtEndereco.Name = "txtEndereco";
+            this.txtEndereco.Size = new System.Drawing.Size(385, 20);
+            this.txtEndereco.TabIndex = 4;
+            // 
+            // txtNome
+            // 
+            this.txtNome.CharacterCasing = System.Windows.Forms.CharacterCasing.Upper;
+            this.txtNome.Location = new System.Drawing.Point(96, 45);
+            this.txtNome.MaxLength = 50;
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(481, 20);
+            this.txtNome.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(44, 29);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Código";
+            // 
+            // txtCodigo
+            // 
+            this.txtCodigo.Enabled = false;
+            this.txtCodigo.Location = new System.Drawing.Point(19, 45);
+            this.txtCodigo.MaxLength = 10;
+            this.txtCodigo.Name = "txtCodigo";
+            this.txtCodigo.ReadOnly = true;
+            this.txtCodigo.Size = new System.Drawing.Size(71, 20);
+            this.txtCodigo.TabIndex = 0;
+            this.txtCodigo.TabStop = false;
+            this.txtCodigo.TextAlign = System.Windows.Forms.HorizontalAlignment.Right;
+            // 
+            // errErro
+            // 
+            this.errErro.ContainerControl = this;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(32, 32);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tstSalvar,
+            this.tstExcluir,
+            this.tstPesquisar,
+            this.toolStripSeparator1,
+            this.tstSair});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(619, 25);
+            this.toolStrip1.TabIndex = 2;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tstSalvar
+            // 
+            this.tstSalvar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tstSalvar.Image = ((System.Drawing.Image)(resources.GetObject("tstSalvar.Image")));
+            this.tstSalvar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tstSalvar.Name = "tstSalvar";
+            this.tstSalvar.Size = new System.Drawing.Size(42, 22);
+            this.tstSalvar.Text = "&Salvar";
+            this.tstSalvar.ToolTipText = "Clique para Salvar";
+            this.tstSalvar.Click += new System.EventHandler(this.tstSalvar_Click);
+            // 
+            // tstExcluir
+            // 
+            this.tstExcluir.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tstExcluir.Image = ((System.Drawing.Image)(resources.GetObject("tstExcluir.Image")));
+            this.tstExcluir.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tstExcluir.Name = "tstExcluir";
+            this.tstExcluir.Size = new System.Drawing.Size(45, 22);
+            this.tstExcluir.Text = "&Excluir";
+            this.tstExcluir.ToolTipText = "Excluir";
+            // 
+            // tstPesquisar
+            // 
+            this.tstPesquisar.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tstPesquisar.Image = ((System.Drawing.Image)(resources.GetObject("tstPesquisar.Image")));
+            this.tstPesquisar.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tstPesquisar.Name = "tstPesquisar";
+            this.tstPesquisar.Size = new System.Drawing.Size(61, 22);
+            this.tstPesquisar.Text = "&Pesquisar";
+            this.tstPesquisar.ToolTipText = "Clique para Salvar";
+            // 
+            // toolStripSeparator1
+            // 
+            this.toolStripSeparator1.Name = "toolStripSeparator1";
+            this.toolStripSeparator1.Size = new System.Drawing.Size(6, 25);
+            // 
+            // tstSair
+            // 
+            this.tstSair.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.tstSair.Image = ((System.Drawing.Image)(resources.GetObject("tstSair.Image")));
+            this.tstSair.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tstSair.Name = "tstSair";
+            this.tstSair.Size = new System.Drawing.Size(30, 22);
+            this.tstSair.Text = "S&air";
+            this.tstSair.ToolTipText = "Clique para Salvar";
+            this.tstSair.Click += new System.EventHandler(this.tstSair_Click);
+            // 
+            // frmAgenda
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(619, 307);
+            this.ControlBox = false;
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.grpClientes);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.KeyPreview = true;
+            this.Name = "frmAgenda";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = ".:Agenda de Contatos.:";
+            this.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.frmAgenda_KeyPress);
+            this.grpClientes.ResumeLayout(false);
+            this.grpClientes.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.errErro)).EndInit();
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox grpClientes;
+        private System.Windows.Forms.Label label9;
+        private System.Windows.Forms.MaskedTextBox mskCelular;
+        private System.Windows.Forms.Button btnCEP;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label lblNome;
+        private System.Windows.Forms.MaskedTextBox mskCEP;
+        private System.Windows.Forms.ComboBox cboEstado;
+        private System.Windows.Forms.TextBox txtCidade;
+        private System.Windows.Forms.TextBox txtNumero;
+        private System.Windows.Forms.TextBox txtEndereco;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCodigo;
+        private System.Windows.Forms.ErrorProvider errErro;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripButton tstSalvar;
+        private System.Windows.Forms.ToolStripButton tstExcluir;
+        private System.Windows.Forms.ToolStripButton tstPesquisar;
+        private System.Windows.Forms.ToolStripSeparator toolStripSeparator1;
+        private System.Windows.Forms.ToolStripButton tstSair;
+        internal System.Windows.Forms.TextBox txtBairro;
+    }
+}
+
---
+++ Agenda/Formularios/frmAgenda.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Agenda
+{
+    public partial class frmAgenda : Form
+    {
+        public frmAgenda()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCEP_Click(object sender, EventArgs e)
+        {
+            //chama o método PesquisarCEP
+            PesquisarCEP(mskCEP.Text);
+        }
+
+        private void frmAgenda_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            //envia TAB quando for pressionado enter
+            if ((e.KeyChar.CompareTo((char)Keys.Return)) == 0)
+            {
+                e.Handled = true;
+                SendKeys.Send("{TAB}");
+            }
+        }
+
+        private void tstSair_Click(object sender, EventArgs e)
+        {
+            //fecha o formulário
+            Close();
+        }
+
+        private void tstSalvar_Click(object sender, EventArgs e)
+        {
+            //validação do conteúdo
+            if (txtNome.Text == "") 
+            {
+                errErro.SetError(lblNome, "Campo Obrigatório");
+                return;
+            }
+            else
+            {
+                errErro.SetError(lblNome, "");
+            }
+
+            string conexao = Properties.Settings.Default.conexaoDB;
+
+
+        }
+
+        //PesquisarCEP é um método
+        //string CEP é o argumento
+        //ou assinatura do método
+        private void PesquisarCEP(string CEP)
+        {
+            //pesquisa de CEP
+            DataSet ds = new DataSet();
+
+            string xml = "http://cep.republicavirtual.com.br/web_cep.php?cep=@cep&formato=xml".Replace("@cep", CEP);
+            ds.ReadXml(xml);
+            if (ds.Tables[0].Rows[0]["resultado_txt"].ToString() == "sucesso - cep completo" || ds.Tables[0].Rows[0]["resultado_txt"].ToString() == "sucesso - cep único")
+            {
+                txtEndereco.Text = ds.Tables[0].Rows[0]["tipo_logradouro"].ToString() + " " + ds.Tables[0].Rows[0]["logradouro"].ToString();
+                txtBairro.Text = ds.Tables[0].Rows[0]["bairro"].ToString();
+                txtCidade.Text = ds.Tables[0].Rows[0]["cidade"].ToString();
+                cboEstado.Text = ds.Tables[0].Rows[0]["uf"].ToString();
+                txtNumero.Focus();
+            }
+            else
+            {
+                MessageBox.Show("CEP não Encontrado!", "Atenção", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }             
+    }
+}
